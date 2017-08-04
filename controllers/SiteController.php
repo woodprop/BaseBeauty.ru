6e57d8165d5c43783b7d325ddb8cedf0d7037963
Konstantin <woodprop@yandex.ru>
2017-08-04 11:05:45 +0300

    Реализован вывод трёх последних новостей на главной

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\models\News;
 use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
@@ -61,7 +62,8 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $news = News::find()->limit('3')->orderBy('id DESC')->all();
+        return $this->render('index', ['news' => $news]);
     }
 
     /**
--- views/site/index.php
+++ views/site/index.php
@@ -1,5 +1,4 @@
 <?php
-
 /* @var $this yii\web\View */
 
 $this->title = 'Base Beauty – художественно-производственные мастерские по изготовлению студийных и татральных декораций';
@@ -21,40 +20,19 @@ $this->title = 'Base Beauty – художественно-производст�
     </div>
 
     <div>
-
+        <!-- --------------------------- START NEWS SECTION---------------------------- -->
         <div class="row bb-color-bg">
-            <div class="col-lg-4">
-                <h2>Новость 1</h2>
+            <?php foreach ($news as $new): ?>
+                <div class="col-lg-4">
+                    <h2><?= $new->title; ?></h2>
 
-                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                    <p class="text-justify"><?= \yii\helpers\StringHelper::truncate($new->text,300,'...'); ?></p>
 
-                <p><a class="btn btn-outline btn-block" href="#">Подробнее</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Новость 2</h2>
-
-                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline btn-block" href="#">Подробнее</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Новость 3</h2>
-
-                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline btn-block" href="#">Подробнее</a></p>
-            </div>
+                    <p><a class="btn btn-outline btn-block" href="#">Подробнее</a></p>
+                </div>
+            <?php endforeach; ?>
         </div>
-
+        <!-- --------------------------- END NEWS SECTION---------------------------- -->
         <div class="row project-stages">
             <div class="col-lg-4">
 
