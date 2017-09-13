d1cf8845e9ad21462425d9ab05f07e504a18af6d
Konstantin <woodprop@yandex.ru>
2017-09-13 14:45:33 +0300

    Скрыта кнопка Login

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -43,18 +43,18 @@ AppAsset::register($this);
 //            ['label' => 'Проекты', 'url' => ['/site/projects']],
             ['label' => 'Контакты', 'url' => ['/site/contact']],
 //            ['label' => 'Фотогалерея', 'url' => ['/site/gallery']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+//            Yii::$app->user->isGuest ? (
+//                ['label' => 'Login', 'url' => ['/site/login']]
+//            ) : (
+//                '<li>'
+//                . Html::beginForm(['/site/logout'], 'post')
+//                . Html::submitButton(
+//                    'Logout (' . Yii::$app->user->identity->username . ')',
+//                    ['class' => 'btn btn-link logout']
+//                )
+//                . Html::endForm()
+//                . '</li>'
+//            )
         ],
     ]);
     NavBar::end();
